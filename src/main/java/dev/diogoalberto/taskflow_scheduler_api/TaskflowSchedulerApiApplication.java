8b66ed2ae0c674a836627b902165e2c8e7f60f6f
Diogo Alberto <diogoalberto.dev@gmail.com>
2025-10-01 08:11:34 -0300

    initial commit

---
+++ src/main/java/dev/diogoalberto/taskflow_scheduler_api/TaskflowSchedulerApiApplication.java
@@ -0,0 +1,13 @@
+package dev.diogoalberto.taskflow_scheduler_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskflowSchedulerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskflowSchedulerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/diogoalberto/taskflow_scheduler_api/TaskflowSchedulerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.diogoalberto.taskflow_scheduler_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskflowSchedulerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
