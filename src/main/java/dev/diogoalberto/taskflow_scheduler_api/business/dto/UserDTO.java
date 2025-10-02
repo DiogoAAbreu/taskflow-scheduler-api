7614dac7c0a0b58dff4387061f4385a586b0d438
Diogo Alberto <diogoalberto.dev@gmail.com>
2025-10-02 11:19:19 -0300

    feat(security): added spring security settings

---
+++ src/main/java/dev/diogoalberto/taskflow_scheduler_api/business/dto/UserDTO.java
@@ -0,0 +1,15 @@
+package dev.diogoalberto.taskflow_scheduler_api.business.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserDTO {
+    String email;
+    String password;
+}
---
+++ src/main/java/dev/diogoalberto/taskflow_scheduler_api/infrastructure/client/UserClient.java
@@ -0,0 +1,14 @@
+package dev.diogoalberto.taskflow_scheduler_api.infrastructure.client;
+
+import dev.diogoalberto.taskflow_scheduler_api.business.dto.UserDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "taskflow-user", url = "${taskflow.user.url}")
+public interface UserClient {
+    @GetMapping
+    UserDTO getUserByEmail(@RequestParam("email") String email,
+                           @RequestHeader("Authorization") String token);
+}
